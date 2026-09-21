fc87d4ab156694c39f8dce3ef267152e938aff45
satyam5raj <satyamraj529@gmail.com>
2026-09-21 20:41:47 +0530

    Parking-lot-design-lld-node

--- Node/Parking-lot-lld/main.ts
+++ Node/Parking-lot-lld/main.ts
@@ -59,51 +59,51 @@
 
 
 enum VehicleType {
-  MOTORCYCLE = 'MOTORCYCLE',
-  CAR = 'CAR',
-  TRUCK = 'TRUCK',
+    MOTORCYCLE = 'MOTORCYCLE',
+    CAR = 'CAR',
+    TRUCK = 'TRUCK',
 }
 
 enum SpotType {
-  MOTORCYCLE = 'MOTORCYCLE',
-  COMPACT = 'COMPACT',
-  LARGE = 'LARGE',
+    MOTORCYCLE = 'MOTORCYCLE',
+    COMPACT = 'COMPACT',
+    LARGE = 'LARGE',
 }
 
 enum TicketStatus {
-  ACTIVE = 'ACTIVE',
-  COMPLETED = 'COMPLETED',
+    ACTIVE = 'ACTIVE',
+    COMPLETED = 'COMPLETED',
 }
 
 enum PaymentStatus {
-  PENDING = 'PENDING',
-  SUCCESS = 'SUCCESS',
-  FAILED = 'FAILED',
+    PENDING = 'PENDING',
+    SUCCESS = 'SUCCESS',
+    FAILED = 'FAILED',
 }
 
 abstract class Vehicle {
     constructor(
         public readonly licensePlate: string,
         public readonly type: VehicleType,
-    ) {}
+    ) { }
 }
 
 class Car extends Vehicle {
-    constructor(licensePlate: string){
+    constructor(licensePlate: string) {
         super(licensePlate, VehicleType.CAR)
     }
 }
 
 class Motorcycle extends Vehicle {
-  constructor(licensePlate: string) {
-    super(licensePlate, VehicleType.MOTORCYCLE);
-  }
+    constructor(licensePlate: string) {
+        super(licensePlate, VehicleType.MOTORCYCLE);
+    }
 }
 
 class Truck extends Vehicle {
-  constructor(licensePlate: string) {
-    super(licensePlate, VehicleType.TRUCK);
-  }
+    constructor(licensePlate: string) {
+        super(licensePlate, VehicleType.TRUCK);
+    }
 }
 
 // const car = new Car('BR01AB1234');
@@ -115,27 +115,27 @@ class Truck extends Vehicle {
 // CAR 
 
 class ParkingSpot {
-    private vehicle? : Vehicle;
+    private vehicle?: Vehicle;
 
     constructor(
         public readonly id: string,
         public readonly floorId: string,
-        public readonly type: SpotType 
-    ) {}
+        public readonly type: SpotType
+    ) { }
 
     isAvailable(): boolean {
         return this.vehicle === undefined;
     }
 
     park(vechile: Vehicle): void {
-        if(!this.isAvailable()){
+        if (!this.isAvailable()) {
             throw new Error(`Spot ${this.id} is already occupied`);
         }
         this.vehicle = vechile;
     }
 
     release(): void {
-        if(this.isAvailable()){
+        if (this.isAvailable()) {
             throw new Error(`Spot ${this.id} is already empty`);
         }
         this.vehicle = undefined;
@@ -152,10 +152,10 @@ class ParkingFloor {
 
     constructor(
         public readonly id: string
-    ) {}
+    ) { }
 
     addSpot(spot: ParkingSpot): void {
-        if(this.spots.has(spot.id)){
+        if (this.spots.has(spot.id)) {
             throw new Error(`Spot ${spot.id} already exists`)
         }
         this.spots.set(spot.id, spot)
@@ -165,5 +165,94 @@ class ParkingFloor {
         return this.spots.get(id);
     }
 
+    getSpots(): ParkingSpot[] {
+        return [...this.spots.values()];
+    }
 
+    getAvailableSpots(): ParkingSpot[] {
+        return this.getSpots().filter(
+            spot => spot.isAvailable(),
+        )
+    }
 }
+
+
+class ParkingTicket {
+    public readonly entryTime: Date;
+
+    private exitTime?: Date;
+    private status: TicketStatus = TicketStatus.ACTIVE;
+
+    constructor(
+        public readonly id: string,
+        public readonly vehicle: Vehicle,
+        public readonly spot: ParkingSpot,
+    ) {
+        this.entryTime = new Date()
+    }
+
+    close(exitTime: Date = new Date()): void {
+        if (this.status !== TicketStatus.ACTIVE) {
+            throw new Error(
+                'Ticket is already completed',
+            );
+        }
+
+        this.exitTime = exitTime;
+        this.status = TicketStatus.COMPLETED;
+    }
+}
+
+class ParkingLot {
+  constructor(
+    public readonly id: string,
+    private readonly floors: ParkingFloor[],
+  ) {}
+
+  addFloor(floor: ParkingFloor): void {
+    this.floors.push(floor);
+  }
+
+  getFloors(): ParkingFloor[] {
+    return [...this.floors];
+  }
+}
+
+const floor1 = new ParkingFloor('F1');
+
+const compactSpot =
+  new ParkingSpot(
+    'C1',
+    'F1',
+    SpotType.COMPACT,
+  );
+
+const largeSpot =
+  new ParkingSpot(
+    'L1',
+    'F1',
+    SpotType.LARGE,
+  );
+
+floor1.addSpot(compactSpot);
+floor1.addSpot(largeSpot);
+
+const parkingLot =
+  new ParkingLot(
+    'PL1',
+    [floor1],
+  );
+
+const car =
+  new Car('BR01AB1234');
+
+compactSpot.park(car);
+
+const ticket =
+  new ParkingTicket(
+    'TICKET-001',
+    car,
+    compactSpot,
+  );
+
+
